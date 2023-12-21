89864f458ce252c0e6377031a47aa2427d9a3968
Shahreyar Tonmoy <shahreyartonmoy001@gmail.com>
2023-12-21 10:54:09 +0600

    icon set

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -52,7 +52,7 @@ const Navbar = () => {
 
     return (
         <div>
-            <div className="navbar lg:px-8  ">
+            <div className="navbar lg:px-8">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
